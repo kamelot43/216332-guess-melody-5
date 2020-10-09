7f504a93b1fd121eced82cf4ee8f985f898703e1
admin <admin@beebops-MacBook-Pro.local>
2020-10-10 00:12:02 +0300

    рефакторинг компонетов

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -37,55 +37,46 @@ class GenreQuestionScreen extends PureComponent {
         </header>
 
         <section className="game__screen">
-          <h2 className="game__title">Выберите инди-рок треки</h2>
-          {/*
-          <form className="game__tracks">
-            <div className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-1" />
-                <label className="game__check" htmlFor="answer-1">Отметить</label>
-              </div>
-            </div>
+          <h2 className="game__title">Выберите {genre}треки</h2>
 
-            <div className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-2" />
-                <label className="game__check" htmlFor="answer-2">Отметить</label>
-              </div>
-            </div>
+          <form
+            className="game__tracks"
+            onSubmit={(evt) => {
+              evt.preventDefault();
+              onAnswer(question, this.state.answers);
+            }}
 
-            <div className="track">
-              <button className="track__button track__button--pause" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-3" />
-                <label className="game__check" htmlFor="answer-3">Отметить</label>
-              </div>
-            </div>
+          >
 
-            <div className="track">
-              <button className="track__button track__button--play" type="button"></button>
-              <div className="track__status">
-                <audio></audio>
-              </div>
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-4" />
-                <label className="game__check" htmlFor="answer-4">Отметить</label>
-              </div>
-            </div>
+
+             {answers.map((answer, i) => (
+               <div key={`${i}-${answer.src}`} className="track">
+                 <button className="track__button track__button--play" type="button"></button>
+                 <div className="track__status">
+                   <audio
+                     src={answer.src}
+                   />
+                 </div>
+                 <div className="game__answer">
+                   <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`}
+                    id={`answer-${i}`}
+                    checked={userAnswers[i]}
+                    onChange={(evt) => {
+                      const value = evt.target.checked;
+
+                      this.setState({
+                        answers: [...userAnswers.slice(0,i), value, ...userAnswers.slice(i+1)],
+                      })
+                    }}
+
+                   />
+                   <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
+                 </div>
+               </div>
+             ))}
 
             <button className="game__submit button" type="submit">Ответить</button>
-          </form>*/}
+          </form>
         </section>
       </section>
     )
