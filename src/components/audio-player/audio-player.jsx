fd8060d04bd08ce35cf59233a25645ffe368e637
admin <admin@beebops-MacBook-Pro.local>
2020-10-18 22:46:56 +0300

    создание компонента audio-player

---
+++ src/components/audio-player/audio-player.jsx
@@ -0,0 +1,78 @@
+import React, {PureComponent, Fragment, createRef} from "react";
+import PropTypes from "prop-types";
+
+export default class AudioPlayer extends PureComponent {
+
+  constructor(props) {
+    super(props);
+
+    this._audioRef = createRef();
+
+    this.state = {
+      isLoading: true,
+      isPlaying: props.isPlaying,
+    };
+  }
+
+  componentDidMount() {
+    const {src} = this.props;
+    const audio = this._audioRef.current;
+
+    audio.src = src;
+
+    audio.oncanplaythrough = () => this.setState({
+      isLoading: false,
+    });
+
+    audio.onplay = () => {
+      this.setState({
+        isPlaying: true,
+      });
+    };
+
+    audio.onpause = () => this.setState({
+      isPlaying: false,
+    });
+  }
+
+  componentWillUnmount() {
+    const audio = this._audioRef.current;
+    audio.oncanplaythrough = null;
+    audio.onplay = null;
+    audio.onpause = null;
+  }
+
+  render() {
+     const {isLoading, isPlaying} = this.state;
+
+     return (
+       <Fragment>
+         <button
+           className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
+           type="button"
+           disabled={isLoading}
+           onClick={() => this.setState({isPlaying: !this.state.isPlaying})}
+         />
+
+         <div className="track__status">
+           <audio
+             ref={this._audioRef}
+           />
+         </div>
+       </Fragment>
+     );
+  }
+
+  componentDidUpdate() {
+    if (this.state.isPlaying) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+}
+
+AudioPlayer.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+};
